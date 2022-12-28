a6eec9b9f2ea797b43faad41792e83ffc6ae0bfd
Anwarul Islam <anwaarulislaam@gmail.com>
2022-12-29 03:43:09 +0600

    settings done

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -2,14 +2,26 @@
   <div class="setting-flex x-flex x-justify-center x-gap-4">
     <Checkbox :checked="option.mic" label="Microphone" id="mic">
       <span style="margin-left: -4px">
-        <img class="active image-icon" src="/mic.png" />
-        <img class="inactive image-icon" src="/mic-n.png" />
+        <img
+          class="active image-icon"
+          src="https://cdn.jsdelivr.net/gh/Ace-Tinker/screen-recorder@gh-pages/mic.png"
+        />
+        <img
+          class="inactive image-icon"
+          src="https://cdn.jsdelivr.net/gh/Ace-Tinker/screen-recorder@gh-pages/mic-n.png"
+        />
       </span>
     </Checkbox>
     <Checkbox :checked="option.audio" label="Sound" id="audio">
       <span style="margin-left: -4px">
-        <img class="active image-icon" src="/sound.png" />
-        <img class="inactive image-icon" src="/sound-n.png" />
+        <img
+          class="active image-icon"
+          src="https://cdn.jsdelivr.net/gh/Ace-Tinker/screen-recorder@gh-pages/sound.png"
+        />
+        <img
+          class="inactive image-icon"
+          src="https://cdn.jsdelivr.net/gh/Ace-Tinker/screen-recorder@gh-pages/sound-n.png"
+        />
       </span>
     </Checkbox>
   </div>
